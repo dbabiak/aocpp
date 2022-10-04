89f3fe68b3a885439b34ccbac4e2434f792d281c
dbabiak <daniel@wave.com>
2022-10-04 20:02:41 +0000

    d01

---
+++ utils.h
@@ -0,0 +1,9 @@
+//
+// Created by Daniel Babiak on 2022-10-04.
+//
+
+#ifndef AOCPP_UTILS_H
+#define AOCPP_UTILS_H
+
+std::vector<int> parse(const std:: vector<std::string> &xs);
+#endif //AOCPP_UTILS_H
